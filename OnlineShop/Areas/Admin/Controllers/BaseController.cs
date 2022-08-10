82d0dc62cdadd41ed1010072ac2068a3494c48ce
hiepnxph <hiepnxph@gmail.com>
2022-08-10 15:09:31 +0700

    Add project files.

---
+++ OnlineShop/Areas/Admin/Controllers/BaseController.cs
@@ -0,0 +1,25 @@
+﻿using OnlineShop.Commom;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace OnlineShop.Areas.Admin.Controllers
+{
+    public class BaseController : Controller
+    {
+        // GET: Admin/Base
+        protected override void OnActionExecuting (ActionExecutingContext filerContext)
+        {
+            var seccsion = (UserLogin)Session[CommomConstants.USER_SESSION];
+            if(seccsion == null)
+            {
+                filerContext.Result = new RedirectToRouteResult(new
+                    RouteValueDictionary(new { controller = "Login", action = "index", area = "admin" }));
+            }
+            base.OnActionExecuting (filerContext);
+        }
+    }
+}
---
+++ OnlineShop/Areas/Admin/Models/LoginModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OnlineShop.Areas.Admin.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Please enter your full name")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Please enter the full password")]
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
+
+    }
+}
